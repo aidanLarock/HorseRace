8f46ccb170e2aadfac210b3e047275f6d680dc4c
A-Larock <git@aidanlarock.com>
2022-04-06 22:59:26 -0400

    yuh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Popup from './Popup';
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
 
   return (
     <div className="App">
@@ -12,17 +20,27 @@ function App() {
           Number of Players: 
         </h3>
         <input type="text" name="name" />
-        <button>Start</button>
+        <button>➜</button>
         </div>
         <br></br>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          How to Play
-        </a>
+        <input
+          type="button"
+          value="How To Play?"
+          onClick={togglePopup}
+        />
+
+      {isOpen && <Popup
+      content={<>
+        <b>How To Play Horse Racing</b>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <input
+          type="button"
+          value="Got It!"
+          onClick={togglePopup}
+        />
+      </>}
+        handleClose={togglePopup}
+      />}
       </header>
     </div>
   );
